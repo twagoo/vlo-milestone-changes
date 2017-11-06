2105d4ab8d2377e7e1b3cef535a3532c44796430
Twan Goosen <twan@clarin.eu>
2017-11-06 10:30:13 +0100

    jq script: print milestone title and formatting of issues and urls

--- retrieve-jq.sh
+++ retrieve-jq.sh
@@ -1,10 +1,13 @@
 #/bin/sh
 
-#this uses jq; use 'brew install jq' on Mac
+# This uses jq; see https://stedolan.github.io/jq/
+# Use 'brew install jq' on Mac
 
 MILESTONE="4"
 STATE="all"
 
-URI="https://api.github.com/repos/clarin-eric/VLO/issues?milestone=${MILESTONE}&state=${STATE}"
+MILESTONE_URI="https://api.github.com/repos/clarin-eric/VLO/milestones/${MILESTONE}"
+ISSUES_URI="https://api.github.com/repos/clarin-eric/VLO/issues?milestone=${MILESTONE}&state=${STATE}"
 
-curl -s $URI | jq -r ".[]|.title,.html_url"
+curl -s "${MILESTONE_URI}" | jq -r ".title"
+curl -s "$ISSUES_URI" | jq -r ".[] | (\"* \" + .title), \"    <\" + .html_url + \">\""
